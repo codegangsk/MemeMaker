9f0a9f0183c6f762f45fe91359f3d75d0bce8414
Sophie Kim <kjhj0604@gmail.com>
2020-07-12 08:52:08 -0700

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by Sophie Kim on 2020/07/12.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
